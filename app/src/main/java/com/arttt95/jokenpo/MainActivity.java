bc9ede3ef420cd856dd316f51e598eb7ccd6695e
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-28 20:56:48 -0300

    28NOV24 - Aula 52 AS - #App Jokenpô (Instaurando os métodos -> onClick).

--- app/src/main/java/com/arttt95/jokenpo/MainActivity.java
+++ app/src/main/java/com/arttt95/jokenpo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.arttt95.jokenpo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +13,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        this.setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+    }
+
+    public void selecionarPedra(View view ) {
+
+
+        verificarGanhador("Pedra");
+
+    }
+
+    public void selecionarPapel(View view ) {
+
+        verificarGanhador("Papel");
+
+    }
+
+    public void selecionarTesoura(View view ) {
+
+        verificarGanhador("Tesoura");
+
+    }
+
+    private void verificarGanhador(String escolhaUsuario) {
+
+        System.out.println("Item clicado: " + escolhaUsuario);
+
     }
 }
